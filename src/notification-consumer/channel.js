aa7a9945ee34d4fc279f06c64ec45e2f1eda0a51
Mahmoud Soliman <Mahmoud.soliman@vezeeta.com>
2019-03-29 22:29:44 +0200

    consumer working

---
+++ src/notification-consumer/channel.js
@@ -0,0 +1,36 @@
+var amqp = require('amqplib/callback_api');
+var url = process.env.AMQP_URL || 'amqp://guest:guest@localhost:32785';
+module.exports = createQueueChannel;
+function createQueueChannel(queue, cb) {  
+  amqp.connect(url, onceConnected);
+  function onceConnected(err, conn) {
+    if (err) {
+      cb(err);
+    }
+    else {
+      console.log('connected');
+      conn.createChannel(onceChannelCreated);
+    }
+
+    function onceChannelCreated(err, channel) {
+      if (err) {
+        console.log('error creating channel');
+        cb(err);
+      }
+      else {
+          console.log('channel created');
+          channel.assertQueue(queue, {durable: true}, onceQueueCreated);
+      }
+      function onceQueueCreated(err) {
+        if (err) {
+          console.log('error creating queue');
+          cb(err);
+        }
+        else {
+          console.log('queue created');
+          cb(null, channel, conn);
+        }
+      }
+    }
+  }
+}
